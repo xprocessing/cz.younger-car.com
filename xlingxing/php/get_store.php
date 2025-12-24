4e19457842c6455dd13829c81c167f7e2c8c3818
xprocessing <xprocessing@live.com>
2025-12-24 16:42:44 +0800

    get_store

---
+++ xlingxing/php/get_store.php
@@ -0,0 +1,37 @@
+<?php
+// ========== 响应头配置 ==========
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json; charset=utf-8"); // 改为JSON输出
+//引入lx_api.php
+
+require_once __DIR__ . '/lx_api.php';
+try {
+    // 初始化API客户端
+    $apiClient = new LingXingApiClient();
+    //当前时间戳，按秒
+    $currentTimestamp = time();
+    //前天的时间戳，按秒
+    $twoDaysAgoTimestamp = $currentTimestamp - (5 * 24 * 60 * 60);
+    //格式化为日期时间字符串
+
+   
+    // 调用POST接口示例
+    $orderParams = [
+        'offset' => 0,
+        'length' => 100,       
+        'is_sync' => 1,
+        'status' => 1 
+    ];
+    $orders = $apiClient->post('/pb/mp/shop/v2/getSellerList', $orderParams);
+    //print_r("订单数据：" . PHP_EOL);
+    //json格式化输出
+    echo json_encode($orders, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+} catch (\Exception $e) {
+    echo "错误：" . $e->getMessage() . PHP_EOL;
+}
+
+
+
+?>
