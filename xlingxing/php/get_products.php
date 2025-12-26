bcb9bd494a803fefb8661a58c458c80e5bd7f6d3
xprocessing <xprocessing@live.com>
2025-12-26 11:00:59 +0800

    抓产品

---
+++ xlingxing/php/get_products.php
@@ -0,0 +1,36 @@
+<?php
+// ========== 响应头配置 ==========
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json; charset=utf-8"); // 改为JSON输出
+//引入lx_api.php
+
+require_once __DIR__ . '/lx_api.php';
+try {
+    // 初始化API客户端
+    $apiClient = new LingXingApiClient();
+    //当前时间戳，按秒
+    $currentTimestamp = time();
+    //前天的时间戳，按秒
+    $twoDaysAgoTimestamp = $currentTimestamp - (5 * 24 * 60 * 60);
+    //格式化为日期时间字符串
+
+   
+    // 调用POST接口示例
+    $orderParams = [
+        'offset' => 0,
+        'length' => 100       
+       
+    ];
+    $orders = $apiClient->post('/erp/sc/routing/data/local_inventory/productList', $orderParams);
+    //print_r("订单数据：" . PHP_EOL);
+    //json格式化输出
+    echo json_encode($orders, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+} catch (\Exception $e) {
+    echo "错误：" . $e->getMessage() . PHP_EOL;
+}
+
+
+
+?>
