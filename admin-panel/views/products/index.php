3bf2fcbdbcb876fda14b74b91236ee02ee56bf1f
xprocessing <xprocessing@live.com>
2025-12-29 11:58:10 +0800

    增加边框

--- admin-panel/views/products/index.php
+++ admin-panel/views/products/index.php
@@ -244,5 +244,6 @@ document.getElementById('selectAll').addEventListener('change', function() {
     transform: scale(5);
     z-index: 1000;
     position: relative;
+    border: 2px solid #000;
 }
 </style>
