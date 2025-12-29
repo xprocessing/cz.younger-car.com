a1ddc1487280e8cd15d07335094652788cc53cae
xprocessing <xprocessing@live.com>
2025-12-29 16:24:45 +0800

    11

--- xlingxing/php/get_orders_amazon.php
+++ xlingxing/php/get_orders_amazon.php
@@ -13,7 +13,7 @@ try {
     //当前时间戳，按 Y-m-d H:i:s 格式
     $end_date = date('Y-m-d H:i:s');
     //前天的时间 ，按 Y-m-d H:i:s 格式
-    $start_date = date('Y-m-d H:i:s', strtotime('-1 days'));
+    $start_date = date('Y-m-d H:i:s', strtotime('-2 days'));
     //格式化为日期时间字符串
 
    
@@ -21,7 +21,7 @@ try {
     $orderParams = [
         'offset' => 0,
         'length' => 200,       
-        'date_type' => 1, //1 订购时间【站点时间】 2 订单修改时间【北京时间】 3 平台更新时间【UTC时间】
+        'date_type' => 2, //1 订购时间【站点时间】 2 订单修改时间【北京时间】 3 平台更新时间【UTC时间】
         'start_date' => $start_date, //查询时间，左闭右开，格式：Y-m-d 或 Y-m-d H:i:s
         'end_date' => $end_date,
         'fulfillment_channel' => 1 // 1表示FBA订单，2表示FBM订单
