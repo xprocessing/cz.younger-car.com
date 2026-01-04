62b9c39a7c45789e91b3211fabe2a60c326efa61
xprocessing <xprocessing@live.com>
2026-01-04 14:36:22 +0800

    获取仓库明细

---
+++ xlingxing/php/get_inventoryDetails.php
@@ -0,0 +1,33 @@
+<?php
+// ========== 响应头配置 ==========
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json; charset=utf-8"); // 改为JSON输出
+//引入lx_api.php
+
+require_once __DIR__ . '/lx_api.php';
+try {
+    // 初始化API客户端
+    $apiClient = new LingXingApiClient();
+
+    $sku = $_GET['sku'];
+   
+    // 调用POST接口示例
+    $orderParams = [
+        'offset' => 0,
+        'length' => 100,
+        'sku' => $sku
+        
+    ];
+    $orders = $apiClient->post('/erp/sc/routing/data/local_inventory/inventoryDetails', $orderParams);
+    //print_r("订单数据：" . PHP_EOL);
+    //json格式化输出
+    echo json_encode($orders, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+} catch (\Exception $e) {
+    echo "错误：" . $e->getMessage() . PHP_EOL;
+}
+
+
+
+?>
