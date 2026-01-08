064ae50970cf1107b342a3563b25b903c324e5f1
xprocessing <xprocessing@live.com>
2026-01-08 16:38:18 +0800

    ok

--- admin-panel/views/car_data/index.php
+++ admin-panel/views/car_data/index.php
@@ -92,7 +92,7 @@
                 </div>
                 <div class="card-body">
                     <div class="table-responsive">
-                        <table class="table table-bordered table-striped">
+                        <table class="table table-striped table-hover">
                             <thead class="table-dark">
                                 <tr>
                                     <th>ID</th>
