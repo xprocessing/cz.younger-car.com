4692af6d04836dda5ae6de4c0732b98424c821e8
xprocessing <xprocessing@live.com>
2026-01-12 16:58:53 +0800

    完善提醒

--- admin-panel/ding_v2.php
+++ admin-panel/ding_v2.php
@@ -200,26 +200,28 @@ $options = [
 
 $db = new PDO($dsn, DB_USER, DB_PASS, $options);
 
-// 1. 从order_profit表 获取昨天昨日亏损超过10美元的订单
+// 1. 从order_profit表 获取昨天昨日亏损超过10美元的订单，以及store_id对应store表中的store_name
 $yesterday = date('Y-m-d', strtotime('-1 day'));
-$sql1 = "SELECT global_order_no, profit_amount, local_sku, global_purchase_time 
-         FROM order_profit 
-         WHERE profit_rate < '0' 
-         AND DATE(STR_TO_DATE(global_purchase_time, '%Y-%m-%d %H:%i:%s')) = :yesterday";
+$sql1 = "SELECT op.global_order_no, op.profit_amount, op.local_sku, op.global_purchase_time, s.store_name
+         FROM order_profit op
+         LEFT JOIN store s ON op.store_id = s.store_id
+         WHERE op.profit_rate < '0' 
+         AND DATE(STR_TO_DATE(op.global_purchase_time, '%Y-%m-%d %H:%i:%s')) = :yesterday";
 $stmt1 = $db->prepare($sql1);
 $stmt1->execute(['yesterday' => $yesterday]);
 $lossOrders = $stmt1->fetchAll();
 
 if (!empty($lossOrders)) {
     $content .= "### 昨日亏损超过10美元的订单\n";
-    $content .= "| 订单号 | 亏损金额 | SKU | 下单时间 |\n";
-    $content .= "| --- | --- | --- | --- |\n";
+    $content .= "| 订单号 | 亏损金额 | SKU | 下单时间 | 店铺名称 |\n";
+    $content .= "| --- | --- | --- | --- | --- |\n";
     foreach ($lossOrders as $order) {
-        $content .= sprintf("| %s | %s | %s | %s |\n", 
+        $content .= sprintf("| %s | %s | %s | %s | %s |\n", 
             $order['global_order_no'], 
             $order['profit_amount'], 
             $order['local_sku'], 
-            $order['global_purchase_time']
+            $order['global_purchase_time'],
+            $order['store_name']
         );
     }
     $content .= "\n";
@@ -245,6 +247,7 @@ if (!empty($oldStockSkus)) {
         );
     }
     $content .= "\n";
+    $content.="更多库存信息，请访问：https://cz.younger-car.com/admin-panel/inventory_details.php?action=inventory_alert";
 }
 
 // 3. 从order_profit和inventory_details表，根据sku的近30天销量，判断库存是否不足
@@ -256,12 +259,9 @@ if (empty($content)) {
     //结束程序
     exit;
 }
-else{
-    $content.="更多库存信息，请访问：https://cz.younger-car.com/admin-panel/inventory_details.php?action=inventory_alert";
-}
 
-
-$mobileList = ["18868725001","18868268995","13868380570"];
+//$mobileList = ["18868725001","18868268995","13868380570"];
+$mobileList = ["18868725001"];
 //$content = "缺货预警：有缺货sku...[点击查看](https://cz.younger-car.com/admin-panel/inventory_details.php?action=inventory_alert)";
 
 // 3. 调用推送方法，获取标准化执行结果
