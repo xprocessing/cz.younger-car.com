6ca5d75e81ba3605d80795b3425c51acf77367cd
xprocessing <xprocessing@live.com>
2026-01-14 17:16:32 +0800

    123

--- xlingxing/php/get_purchaseOrderList2db.php
+++ xlingxing/php/get_purchaseOrderList2db.php
@@ -35,5 +35,22 @@ try {
 }
 
 
+//提取获取的数据 data.data数组中每一数据 对应的  item_list 数组中每一个数据的wid,sku,quantity_receive 组成一个新的数组。
+$newArray = [];
+foreach ($orders['data']['data'] as $order) {
+    foreach ($order['item_list'] as $item) {
+        $newArray[] = [
+            'wid' => $item['wid'],
+            'sku' => $item['sku'],
+            'quantity_receive' => $item['quantity_receive'],
+        ];
+    }
+}
+// 打印新数组
+print_r($newArray);
+
+
+
+
 
 ?>
