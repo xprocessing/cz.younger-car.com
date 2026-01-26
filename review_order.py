3d9fa5634835c609f5d20ba3d07918216d252c8f
xprocessing <xprocessing@live.com>
2026-01-26 18:04:54 +0800

    11111

--- review_order.py
+++ review_order.py
@@ -5,15 +5,10 @@
 # 获取待审核订单列表信息（sku，订单号，店铺id，订单创建时间，订单状态等） https://cz.younger-car.com/xlingxing/php/get_orders.php?nDaysAgo=2 
 # 通过sku查询库存 https://cz.younger-car.com/xlingxing/php/get_inventoryDetails.php?sku=NI-C63-FL-GB
 
-
-
-
-
-# 通过sku获取中邮的产品规格
+# 通过sku获取中邮的产品规格 http://cz.younger-car.com/yunfei_kucun/api_ems/getProductList.php?page=1&pageSize=10&product_sku=YOUR_SKU_001
 
 # 通过sku获取运德的产品规格
 
-
-
 # 获取中邮运费试算api https://cz.younger-car.com/yunfei_kucun/api_ems/ems_api6.php?postcode=90210&weight=1.5&warehouse=USEA,USWE&channel=USPS-PRIORITY,AMAZON-GROUND
 # 获取运德的运费试算 
+
