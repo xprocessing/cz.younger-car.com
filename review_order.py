2ac5a2549816ad7109f52fd49142bafbbe5a15b0
xprocessing <xprocessing@live.com>
2026-01-27 09:52:02 +0800

    增加中邮获取api

--- review_order.py
+++ review_order.py
@@ -6,9 +6,11 @@
 # 通过sku查询库存 https://cz.younger-car.com/xlingxing/php/get_inventoryDetails.php?sku=NI-C63-FL-GB
 
 # 通过sku获取中邮的产品规格 http://cz.younger-car.com/yunfei_kucun/api_ems/getProductList.php?page=1&pageSize=10&product_sku=YOUR_SKU_001
+# 获取中邮运费试算api https://cz.younger-car.com/yunfei_kucun/api_ems/get_ship_fee_api.php?postcode=90210&weight=1.5&warehouse=USEA,USWE&channel=USPS-PRIORITY,AMAZON-GROUND
 
 # 通过sku获取运德的产品规格
-
-# 获取中邮运费试算api https://cz.younger-car.com/yunfei_kucun/api_ems/ems_api6.php?postcode=90210&weight=1.5&warehouse=USEA,USWE&channel=USPS-PRIORITY,AMAZON-GROUND
 # 获取运德的运费试算 
+
+#
+
 
