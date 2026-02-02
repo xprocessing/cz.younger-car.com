9893722b60f0f70eb670aa458e4b01784f89509e
xprocessing <xprocessing@live.com>
2026-02-02 14:44:08 +0800

    111

--- admin-panel/api/api_order_review.php
+++ admin-panel/api/api_order_review.php
@@ -57,7 +57,7 @@ try {
         exit;
     }
     
-    $sql = "INSERT INTO order_review (store_id, global_order_no, local_sku, receiver_country_code, city, postal_code, wd_yunfei, ems_yunfei, wid, logistics_type_id, estimated_yunfei, review_remark) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO order_review (store_id, global_order_no, local_sku, receiver_country_code, city, postal_code, wd_yunfei, ems_yunfei, wid, logistics_type_id, estimated_yunfei, review_remark) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $params = [
         $store_id,
         $global_order_no,
